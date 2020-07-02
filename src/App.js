eccaef762ab9860de65e7188836346ab6c07d0c3
DucAnhN <ducanhn130897@gmail.com>
2020-07-02 20:11:56 +0700

    upload first vesion

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <ul>
+            <li>Go to market</li>
+            <li>Buy food</li>
+            <li>Make dinner</li>
+          </ul>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
